b106bb6da2babb387f2155cb390cfe57492743d8
Arťom Chrolenko <artom.chrolenko@gmail.com>
2024-08-02 14:04:00 +0200

    modified:   inventory/src/Header/Header.css 	modified:   inventory/src/Header/Header.jsx
    commit dropdown buttnu

--- inventory/src/Header/Header.jsx
+++ inventory/src/Header/Header.jsx
@@ -1,25 +1,45 @@
+import "./Header.css";
+function Header({ Title }) {
+  return (
+    <header className="HeaderContainer">
+      <div className="HeaderHeadLine">
+        <h1>{Title}</h1>
+      </div>
+    </header>
+  );
+}
+function Nav() {
+  return (
+    <ul>
+      <li>
+        <a href="#nice">
+          <button>Home</button>
+        </a>
+      </li>
+      <li>
+        <div class="dropdown">
+          <button>Products</button>
+          <div class="dropdown-content">
+            <a href="#link1">Jewellery</a>
+            <a href="#link2">Candles</a>
+            <a href="#link3">Other</a>
+          </div>
+        </div>
+        
+      </li>
 
-import './Header.css'
-function Header({Title}){
-    
-    return (
-        <header className="HeaderContainer">
-            <div className="HeaderHeadLine">
-            <h1>{Title}</h1>
-            </div>
-        </header>
-    );
-    
-    }
-    function Nav(){
-        return (
-                <ul>
-                    <li><a href="#nice"><button className='button-custom'>Home</button></a></li>
-                    <li><a href="#nice"><button>Products</button></a></li>
-                    <li><a href="#nice"><button>Services</button></a></li>
-                    <li><a href="#nice"><button>Contact</button></a></li>
-                </ul>
-        );
-        }
-         
-    export {Header, Nav};
+      <li>
+        <a href="#nice">
+          <button>Services</button>
+        </a>
+      </li>
+      <li>
+        <a href="#nice">
+          <button>Contact</button>
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export { Header, Nav };
